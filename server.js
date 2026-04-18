ead147459e7d2709c89102ebc1bbea02a0e5ec34
Samuuca1 <samuelchaves_2012@hotmail.com>
2026-04-17 22:15:28 -0400

    Add express-validator

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const { connectDB } = require("./mongo");
 require("dotenv").config();
 
-const moviesRouter = require("./movies")
+const moviesRouter = require("./movies");
 const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
-const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
-
-const onServerStart = () => {
-    console.log(`The server is running on http://localhost:${port}`);
-    connectDB();
-};
-
 app.use(moviesRouter);
-app.listen(port, onServerStart);
+
+connectDB();
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`The server is running on http://localhost:${port}`);
+    });
+}
+
+module.exports = app;
+
